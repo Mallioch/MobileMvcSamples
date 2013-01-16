99ad3316267307d090fa0e975973f7ba0d282054
mallioch <Eric Sowell@TX1-ESOWEL06MLV.match.corp>
2013-01-15 21:09:39 -0600

    first resp nav sample

--- MobileMvcSamples/Controllers/ResponsiveNavigationController.cs
+++ MobileMvcSamples/Controllers/ResponsiveNavigationController.cs
@@ -8,7 +8,7 @@ namespace MobileMvcSamples.Controllers
 {
     public class ResponsiveNavigationController : Controller
     {
-        public ActionResult Index()
+        public ActionResult NavMovedToBottom()
         {
             return View();
         }
