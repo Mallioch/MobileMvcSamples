f87352bc76a750ca796712e2a6eecb16b69b199a
mallioch <Eric Sowell@TX1-ESOWEL06MLV.match.corp>
2013-01-18 09:34:38 -0600

    nav to select

--- MobileMvcSamples/Controllers/ResponsiveNavigationController.cs
+++ MobileMvcSamples/Controllers/ResponsiveNavigationController.cs
@@ -17,5 +17,10 @@ namespace MobileMvcSamples.Controllers
         {
             return View();
         }
+
+        public ActionResult NavToSelect()
+        {
+            return View();
+        }
     }
 }
