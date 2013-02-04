bbd0efcc965f20f875ab47f5e70b7d8a42646306
mallioch <Eric Sowell@TX1-ESOWEL06MLV.match.corp>
2013-02-04 17:06:19 -0600

    rearranging files

---
+++ MobileMvcSamples/Controllers/FlexibleNavigationController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MobileMvcSamples.Controllers
+{
+    public class FlexibleNavigationController : Controller
+    {
+        public ActionResult NavMovedToBottom()
+        {
+            return View();
+        }
+
+        public ActionResult NavMovedToBottomNoRepeatedMenu()
+        {
+            return View();
+        }
+
+        public ActionResult NavToSelect()
+        {
+            return View();
+        }
+
+        public ActionResult FlyoutNav()
+        {
+            return View();
+        }
+
+        public ActionResult AccordianNav()
+        {
+            return View();
+        }
+    }
+}
