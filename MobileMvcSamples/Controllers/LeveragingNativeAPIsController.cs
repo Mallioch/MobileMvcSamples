e6a7bd93e0a55d780c5e4bdcfb810d8039500a98
Eric Sowell <eric.sowell@gmail.com>
2013-08-11 15:52:22 -0500

    maps and phone

--- MobileMvcSamples/Controllers/LeveragingNativeAPIsController.cs
+++ MobileMvcSamples/Controllers/LeveragingNativeAPIsController.cs
@@ -46,12 +46,12 @@ namespace MobileMvcSamples.Controllers
                 Android_2_1 = DeviceCoverageAmount.Unknown,
                 Android_2_2 = DeviceCoverageAmount.Unknown,
                 Android_2_3 = DeviceCoverageAmount.Unknown,
-                Android_4_1 = DeviceCoverageAmount.Unknown,
-                Android_4_1_Chrome = DeviceCoverageAmount.Unknown,
-                iOS_4 = DeviceCoverageAmount.Unknown,
-                iOS_5 = DeviceCoverageAmount.Unknown,
-                iOS_6 = DeviceCoverageAmount.Unknown,
-                iOS_7 = DeviceCoverageAmount.Unknown,
+                Android_4_1 = DeviceCoverageAmount.Yes,
+                Android_4_1_Chrome = DeviceCoverageAmount.Yes,
+                iOS_4 = DeviceCoverageAmount.No,
+                iOS_5 = DeviceCoverageAmount.No,
+                iOS_6 = DeviceCoverageAmount.Yes,
+                iOS_7 = DeviceCoverageAmount.Yes,
                 BlackBerry10 = DeviceCoverageAmount.Unknown,
                 Firefox_Android = DeviceCoverageAmount.Unknown,
                 FirefoxOS = DeviceCoverageAmount.Unknown,
@@ -94,7 +94,7 @@ namespace MobileMvcSamples.Controllers
                                            FileAccess.Write,
                                            FileShare.None))
             {
-                //fs.Write(filebytes, 0, filebytes.Length);
+                fs.Write(filebytes, 0, filebytes.Length);
             }
 
 
@@ -127,7 +127,7 @@ namespace MobileMvcSamples.Controllers
                 Tablet_InternetExplorer10 = DeviceCoverageAmount.Yes,
                 KindleFire_FirstGen = DeviceCoverageAmount.Yes,
                 KindleFire_HD = DeviceCoverageAmount.Partial,
-                GalaxyNexus7 = DeviceCoverageAmount.Unknown
+                GalaxyNexus7 = DeviceCoverageAmount.Yes
             };
 
             return View();
@@ -135,11 +135,65 @@ namespace MobileMvcSamples.Controllers
 
         public ActionResult PhoneAndEmail()
         {
+            ViewData[DeviceCoverageHelper.DefaultSetName] = new DeviceCoverage
+            {
+                Android_2_1 = DeviceCoverageAmount.Unknown,
+                Android_2_2 = DeviceCoverageAmount.Unknown,
+                Android_2_3 = DeviceCoverageAmount.Unknown,
+                Android_4_1 = DeviceCoverageAmount.Unknown,
+                Android_4_1_Chrome = DeviceCoverageAmount.Unknown,
+                iOS_4 = DeviceCoverageAmount.Unknown,
+                iOS_5 = DeviceCoverageAmount.Unknown,
+                iOS_6 = DeviceCoverageAmount.Unknown,
+                iOS_7 = DeviceCoverageAmount.Unknown,
+                BlackBerry10 = DeviceCoverageAmount.Unknown,
+                Firefox_Android = DeviceCoverageAmount.Unknown,
+                FirefoxOS = DeviceCoverageAmount.Unknown,
+                Opera_Classic_Android = DeviceCoverageAmount.Unknown,
+                Opera_Webkit_Android = DeviceCoverageAmount.Unknown,
+                WindowsPhone7_5 = DeviceCoverageAmount.Unknown,
+                WindowsPhone8 = DeviceCoverageAmount.Unknown,
+                Desktop_Chrome = DeviceCoverageAmount.Unknown,
+                Desktop_Firefox = DeviceCoverageAmount.Unknown,
+                Desktop_InternetExplorer10 = DeviceCoverageAmount.Unknown,
+                Tablet_InternetExplorer10 = DeviceCoverageAmount.Unknown,
+                KindleFire_FirstGen = DeviceCoverageAmount.Unknown,
+                KindleFire_HD = DeviceCoverageAmount.Unknown,
+                GalaxyNexus7 = DeviceCoverageAmount.Unknown
+            };
+
             return View();
         }
 
         public ActionResult Maps()
         {
+            ViewData[DeviceCoverageHelper.DefaultSetName] = new DeviceCoverage
+            {
+                Android_2_1 = DeviceCoverageAmount.Unknown,
+                Android_2_2 = DeviceCoverageAmount.Unknown,
+                Android_2_3 = DeviceCoverageAmount.Unknown,
+                Android_4_1 = DeviceCoverageAmount.Unknown,
+                Android_4_1_Chrome = DeviceCoverageAmount.Unknown,
+                iOS_4 = DeviceCoverageAmount.Unknown,
+                iOS_5 = DeviceCoverageAmount.Unknown,
+                iOS_6 = DeviceCoverageAmount.Unknown,
+                iOS_7 = DeviceCoverageAmount.Unknown,
+                BlackBerry10 = DeviceCoverageAmount.Unknown,
+                Firefox_Android = DeviceCoverageAmount.Unknown,
+                FirefoxOS = DeviceCoverageAmount.Unknown,
+                Opera_Classic_Android = DeviceCoverageAmount.Unknown,
+                Opera_Webkit_Android = DeviceCoverageAmount.Unknown,
+                WindowsPhone7_5 = DeviceCoverageAmount.Unknown,
+                WindowsPhone8 = DeviceCoverageAmount.Unknown,
+                Desktop_Chrome = DeviceCoverageAmount.Unknown,
+                Desktop_Firefox = DeviceCoverageAmount.Unknown,
+                Desktop_InternetExplorer10 = DeviceCoverageAmount.Unknown,
+                Tablet_InternetExplorer10 = DeviceCoverageAmount.Unknown,
+                KindleFire_FirstGen = DeviceCoverageAmount.Unknown,
+                KindleFire_HD = DeviceCoverageAmount.Unknown,
+                GalaxyNexus7 = DeviceCoverageAmount.Unknown
+            };
+
             return View();
         }
     }
