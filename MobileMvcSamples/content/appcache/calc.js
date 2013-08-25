2ff9eb94e710045334382367a6b84436d60c6cf4
Eric Sowell <eric.sowell@gmail.com>
2013-08-25 17:37:05 -0500

    appcache tweaking 6

---
+++ MobileMvcSamples/content/appcache/calc.js
@@ -0,0 +1,19 @@
+﻿(function () {
+
+  window.addEventListener('load', function () {
+
+    var addButton = document.getElementById('add');
+    add.addEventListener('click', function () {
+
+      var first = document.getElementById('first');
+      var second = document.getElementById('second');
+      var output = document.getElementById('output');
+
+      output.innerText = parseInt(first.value) + parseInt(second.value);
+    });
+    
+
+  });
+  
+
+})();
