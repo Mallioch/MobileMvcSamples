7cb1cea8490068bb0822069ed68b35d64e2f289e
Eric Sowell <eric.sowell@gmail.com>
2013-09-05 20:56:11 -0500

    updating chapter progress

--- MobileMvcSamples/Controllers/LeveragingNewCapabilitiesController.cs
+++ MobileMvcSamples/Controllers/LeveragingNewCapabilitiesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MobileMvcSamples.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -30,6 +31,33 @@ namespace MobileMvcSamples.Controllers
 
         public ActionResult HistoryAPI()
         {
+            ViewData[DeviceCoverageHelper.DefaultSetName] = new DeviceCoverage
+            {
+                Android_2_1 = DeviceCoverageAmount.Unknown,
+                Android_2_2 = DeviceCoverageAmount.Unknown,
+                Android_2_3 = DeviceCoverageAmount.Unknown,
+                Android_4_1 = DeviceCoverageAmount.Unknown,
+                Android_4_1_Chrome = DeviceCoverageAmount.Unknown,
+                iOS_4 = DeviceCoverageAmount.Unknown,
+                iOS_5 = DeviceCoverageAmount.Unknown,
+                iOS_6 = DeviceCoverageAmount.Unknown,
+                iOS_7 = DeviceCoverageAmount.Unknown,
+                BlackBerry10 = DeviceCoverageAmount.Unknown,
+                Firefox_Android = DeviceCoverageAmount.Unknown,
+                FirefoxOS = DeviceCoverageAmount.Unknown,
+                Opera_Classic_Android = DeviceCoverageAmount.Unknown,
+                Opera_Webkit_Android = DeviceCoverageAmount.Unknown,
+                WindowsPhone7_5 = DeviceCoverageAmount.Unknown,
+                WindowsPhone8 = DeviceCoverageAmount.Yes,
+                Desktop_Chrome = DeviceCoverageAmount.Unknown,
+                Desktop_Firefox = DeviceCoverageAmount.Unknown,
+                Desktop_InternetExplorer10 = DeviceCoverageAmount.Unknown,
+                Tablet_InternetExplorer10 = DeviceCoverageAmount.Unknown,
+                KindleFire_FirstGen = DeviceCoverageAmount.Unknown,
+                KindleFire_HD = DeviceCoverageAmount.Unknown,
+                GalaxyNexus7 = DeviceCoverageAmount.Unknown
+            };
+
             return View();
         }
 
