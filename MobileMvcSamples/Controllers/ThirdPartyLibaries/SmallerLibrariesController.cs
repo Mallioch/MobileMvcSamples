2710f0251360a14f3279f15b13a1cdfb795229c4
Eric Sowell <eric.sowell@gmail.com>
2013-09-13 20:20:00 -0500

    third party library stuff

---
+++ MobileMvcSamples/Controllers/ThirdPartyLibaries/SmallerLibrariesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MobileMvcSamples.Controllers.ThirdPartyLibaries
+{
+    public class SmallerLibrariesController : Controller
+    {
+        public ActionResult FitText()
+        {
+            return View(@"~\Views\ThirdPartyLibraries\SmallerLibraries\FitText.cshtml");
+        }
+
+        public ActionResult FlowType()
+        {
+            return View(@"~\Views\ThirdPartyLibraries\SmallerLibraries\FlowType.cshtml");
+        }
+
+        public ActionResult Enquire()
+        {
+            return View(@"~\Views\ThirdPartyLibraries\SmallerLibraries\Enquire.cshtml");
+        }
+
+        [HttpGet]
+        public ActionResult EnquireContent()
+        {
+            return Content("<p>Loaded from the server:</p><img src=\"/content/images/bacon_200.jpg\" />");
+        }
+
+        public ActionResult Respond()
+        {
+            return View(@"~\Views\ThirdPartyLibraries\SmallerLibraries\Respond.cshtml");
+        }
+    }
+}
